0a51ff5ca14ec2890cdd4f054596cf82f6ab685e
Vincent Verhoeven <verhoevenv@gmail.com>
2021-12-10 00:53:17 +0100

    Day 9 part 1

---
+++ src/bin/day9.rs
@@ -0,0 +1,105 @@
+use std::str::FromStr;
+use std::collections::HashMap;
+use std::io;
+use std::io::Read;
+
+#[derive(PartialEq, Eq, Hash, Copy, Clone, Debug)]
+struct XY (Coord, Coord);
+
+type Height = u32;
+type Coord = usize;
+
+pub struct Heightmap {
+    map: HashMap<XY, Height>,
+    size: XY,
+}
+
+impl Heightmap {
+    fn adjacents(&self, p: &XY) -> Vec<(XY, Height)> {
+        let mut adjacents = Vec::new();
+
+        match p {
+            XY(0, y) => adjacents.push(XY(1, *y)),
+            XY(x, y) if *x == (self.size.0 - 1) => adjacents.push(XY(x - 1, *y)),
+            XY(x, y) => adjacents.extend([XY(x - 1, *y), XY(x + 1, *y)].iter()),
+        }
+
+        match p {
+            XY(x, 0) => adjacents.push(XY(*x, 1)),
+            XY(x, y) if *y == (self.size.1 - 1) => adjacents.push(XY(*x, *y - 1)),
+            XY(x, y) => adjacents.extend([XY(*x, *y - 1), XY(*x, *y + 1)].iter()),
+        }
+
+        adjacents.iter().map(|xy| (*xy, *self.map.get(xy).unwrap())).collect() 
+    }
+}
+
+impl FromStr for Heightmap {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut map = HashMap::new();
+
+        let mut size_x = 0;
+        let mut size_y = 0;
+        for (x, l) in s.lines().enumerate() {
+            size_x += 1;
+            size_y = 0;
+            for (y, c) in l.chars().enumerate() {
+                size_y += 1;
+                map.insert(XY(x, y), c.to_digit(10).unwrap());
+            }
+        }
+
+        Ok(Heightmap {map: map, size: XY(size_x, size_y) })
+    }
+}
+
+fn lowpoints(map: &Heightmap) -> Vec<(XY, Height)> {
+    let XY(size_x, size_y) = map.size;
+    let mut result = Vec::new();
+    for x in 0..size_x {
+        for y in 0..size_y {
+            let xy = XY(x,y);
+            let h = map.map.get(&xy).unwrap();
+            if map.adjacents(&xy).iter().all(|(_, ah)| ah > h) {
+                result.push((xy, *h));
+            }
+
+        }
+    }
+    return result;
+}
+
+pub fn sum_risks_lowpoints(map: &Heightmap) -> u32 {
+    lowpoints(map).iter().map(|p| p.1 + 1).sum()
+}
+
+fn main() {
+    let mut input = String::new();
+
+    io::stdin()
+        .read_to_string(&mut input)
+        .expect("Failed to read input");
+
+    let result = sum_risks_lowpoints(&input.parse().unwrap());
+    println!("{}", result);
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sum_risks_lowpoints() {
+        let input = "2199943210
+3987894921
+9856789892
+8767896789
+9899965678";
+
+        assert_eq!(sum_risks_lowpoints(&input.parse().unwrap()), 15);
+    }   
+
+}
